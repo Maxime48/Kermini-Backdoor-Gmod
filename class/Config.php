efbbdf6a5c10aecfb4631989674289af7db1fe82
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2019-02-14 15:39:13 +0100

    Update Config.php

--- class/Config.php
+++ class/Config.php
@@ -1,7 +1,7 @@
 <?php
 // MySQL Configuration
-$GLOBALS['mysql_host'] = "sql.mtxserv.com"; // usually localhost
-$GLOBALS['mysql_database'] = "157071_sql"; // The Database name
-$GLOBALS['mysql_username'] = "w_157071"; // The Username that has access to the database
-$GLOBALS['mysql_password'] = "Ps0820max"; // The Password for the user.
+$GLOBALS['mysql_host'] = ""; // usually localhost
+$GLOBALS['mysql_database'] = ""; // The Database name
+$GLOBALS['mysql_username'] = ""; // The Username that has access to the database
+$GLOBALS['mysql_password'] = ""; // The Password for the user.
 ?>
