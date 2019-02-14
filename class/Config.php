ac64a4f390e304bffd924870474b85bc5f1e3778
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2019-02-14 15:40:09 +0100

    Update Config.php

--- class/Config.php
+++ class/Config.php
@@ -4,4 +4,5 @@ $GLOBALS['mysql_host'] = ""; // usually localhost
 $GLOBALS['mysql_database'] = ""; // The Database name
 $GLOBALS['mysql_username'] = ""; // The Username that has access to the database
 $GLOBALS['mysql_password'] = ""; // The Password for the user.
+// .
 ?>
