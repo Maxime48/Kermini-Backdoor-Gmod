a5fb33c74c5a01234056d84fc86ff3aab35a4251
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2019-02-19 10:28:31 +0100

    Create Kermini-s-q-l.sql

---
+++ Kermini-s-q-l.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+
+-- version 4.8.3
+
+-- https://www.phpmyadmin.net/
+
+--
+
+-- Hôte : localhost
+
+-- Généré le :  lun. 18 fév. 2019 à 23:17
+
+-- Version du serveur :  10.1.37-MariaDB-0+deb9u1
+
+-- Version de PHP :  5.6.37-0+deb8u1
+
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+SET AUTOCOMMIT = 0;
+
+START TRANSACTION;
+
+SET time_zone = "+00:00";
+
+
+
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
+/*!40101 SET NAMES utf8mb4 */;
+
+
+
+
+
+--
+
+-- Structure de la table `accounts`
+
+--
+
+
+
+CREATE TABLE `accounts` (
+
+  `id` int(11) NOT NULL,
+
+  `username` varchar(50) NOT NULL,
+
+  `password` varchar(255) NOT NULL,
+
+  `email` varchar(100) NOT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+--
+
+-- Structure de la table `servers`
+
+--
+
+
+
+CREATE TABLE `servers` (
+
+  `ServerID` int(255) NOT NULL,
+
+  `IPAddress` varchar(255) NOT NULL,
+
+  `Port` varchar(255) NOT NULL,
+
+  `HostName` varchar(255) NOT NULL,
+
+  `Players` varchar(255) NOT NULL,
+
+  `LastUpdate` varchar(255) NOT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+--
+
+-- Index pour les tables déchargées
+
+--
+
+
+
+--
+
+-- Index pour la table `servers`
+
+--
+
+ALTER TABLE `servers`
+
+  ADD PRIMARY KEY (`ServerID`);
+
+
+
+--
+
+-- AUTO_INCREMENT pour les tables déchargées
+
+--
+
+
+
+--
+
+-- AUTO_INCREMENT pour la table `servers`
+
+--
+
+ALTER TABLE `servers`
+
+  MODIFY `ServerID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+COMMIT;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
